822635d5046e03731c852abfc3f79ff3da859b6a
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-12-15 19:16:01 +0200

    Implement Countries List

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
+import CountriesList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -47,7 +48,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
