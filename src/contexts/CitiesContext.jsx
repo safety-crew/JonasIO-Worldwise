7107c7ff05175be4857025681a11887a3c636bad
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-12-25 11:54:58 +0200

    Consume CitiesContext in CityList.jsx and CountryList.jsx

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -38,4 +38,14 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+
+  if (context === undefined) {
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  }
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
